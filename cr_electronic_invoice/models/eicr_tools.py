f4b580750856205768494a02626d6677c506a99b
Nehemias Herrera <yo@nehemias.com>
2025-11-05 09:07:38 +0000

    [FIX] MensajeReceptor v4.4 schemaLocation

--- cr_electronic_invoice/models/eicr_tools.py
+++ cr_electronic_invoice/models/eicr_tools.py
@@ -432,7 +432,7 @@ class ElectronicInvoiceCostaRicaTools(models.AbstractModel):
             schemaLocation = "https://cdn.comprobanteselectronicos.go.cr/xml-schemas/v4.3/mensajeReceptor  https://www.hacienda.go.cr/ATV/ComprobanteElectronico/docs/esquemas/2016/v4.3/MensajeReceptor_V4.3.xsd"
         elif object.company_id.eicr_version_id.name == "v4.4":
             xmlns = "https://cdn.comprobanteselectronicos.go.cr/xml-schemas/v4.4/mensajeReceptor"
-            schemaLocation = "https://cdn.comprobanteselectronicos.go.cr/xml-schemas/v4.4/mensajeReceptor  https://www.hacienda.go.cr/ATV/ComprobanteElectronico/docs/esquemas/2016/v4.4/MensajeReceptor_V4.4.xsd"
+            schemaLocation = "https://cdn.comprobanteselectronicos.go.cr/xml-schemas/v4.4/mensajeReceptor  https://www.hacienda.go.cr/ATV/ComprobanteElectronico/docs/esquemas/2024/v4.4/MensajeReceptor_V4.4.xsd"
         else:
             raise UserError("La versión de facturación electrónica no es válida.")
 
